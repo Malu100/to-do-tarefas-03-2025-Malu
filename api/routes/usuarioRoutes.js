b071c6223d50a85bfab2642ba3b61fce67b613f5
Malu100 <luizaalvesflorencio@gmail.com>
2025-06-17 16:19:55 -0300

    ,

--- api/routes/usuarioRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usuarioController = require('../controllers/usuarioController');
-
-router.post('/usuarios', usuarioController.criarUsuario);
-router.get('/usuarios', usuarioController.listarUsuarios);
-
-module.exports = router;
